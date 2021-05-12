c814662cce273b9c4083c7b1eea75f983e442804
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-12 21:31:56 +0530

    App file updated with router

--- scribble_client/src/App.tsx
+++ scribble_client/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
+// import Footer from './Components/Footer';
+import './Css/root.css';
+
+const Home = lazy(() => import('./Pages/Home'));
+const Login = lazy(() => import('./Pages/Login'));
+const Signup = lazy(() => import('./Pages/Signup'));
+
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Header />
-        Welcome to React with typescript
-      <Footer />
-    </div>
+    <Router>
+      <div className="body_bg">
+        <Header />
+        <Switch>
+          <div className="body_main">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Route path="/" exact component={Home} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/signup" exact component={Signup} />
+            </Suspense>
+          </div>
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   )
 }
 
