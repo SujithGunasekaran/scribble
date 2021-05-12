ef6ecccbc9bb4a562f823f8563edd5d561202af2
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-12 21:34:04 +0530

    Login and signup component added

---
+++ scribble_client/src/Pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Login: React.FC = () => {
+    return (
+        <div>Login Component</div>
+    )
+}
+
+export default Login
---
+++ scribble_client/src/Pages/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Signup: React.FC = () => {
+    return (
+        <div>Login Component</div>
+    )
+}
+
+export default Signup
