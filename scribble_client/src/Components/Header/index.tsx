c1374a030507392dbb56170484a7cb3ca30ef9f8
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-12 21:32:53 +0530

    Header component updated

--- scribble_client/src/Components/Header/index.tsx
+++ scribble_client/src/Components/Header/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../Css/header.css';
 
 const Header: React.FC = () => {
     return (
-        <div>Header Component</div>
+        <div>
+            <div className="header_main">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="header_main_container">
+                                <div className="header_logo anchor_tag">
+                                    <Link to="/" href='/'>Scribble</Link>
+                                </div>
+                                <div className="header_auth_container">
+                                    <div className="header_login anchor_tag">
+                                        <Link to="/login" href="/login">Login</Link>
+                                    </div>
+                                    <div className="header_signup anchor_tag">
+                                        <Link to="/signup" href="/signup">Signup</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
