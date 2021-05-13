31de6a804ea6231f80a702fcfa0039c56a358d72
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-13 21:30:13 +0530

    imported component

---
+++ scribble_client/src/UtilsComponent/index.tsx
@@ -0,0 +1,10 @@
+import { lazy } from 'react';
+
+const editnote = lazy(() => import('../Components/NoteList/EditNote'));
+const preview = lazy(() => import('../Components/NoteList/Preview'));
+
+export const components: any = {
+    editnote,
+    preview
+};
+
