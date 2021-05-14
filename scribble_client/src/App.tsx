a019889a24c0534e52c93af5f0e6dacf5a4a4b2a
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-14 21:35:20 +0530

    App file updated and router moved to index file

--- scribble_client/src/App.tsx
+++ scribble_client/src/App.tsx
@@ -1,33 +1,29 @@
 import React, { Suspense, lazy } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './Css/root.css';
 
 // lazy loading
-
 const Header = lazy(() => import('./Components/Header'));
 const Home = lazy(() => import('./Pages/Home'));
-const Login = lazy(() => import('./Pages/Login'));
-const Signup = lazy(() => import('./Pages/Signup'));
-
+const Form = lazy(() => import('./Pages/Form'));
 
 const App: React.FC = () => {
+
   return (
-    <Router>
-      <div className="body_bg">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Header />
-        </Suspense>
+    <div className="body_bg">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
+      <div className='body_main'>
         <Switch>
-          <div className="body_main">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Route path="/" exact component={Home} />
-              <Route path="/login" exact component={Login} />
-              <Route path="/signup" exact component={Signup} />
-            </Suspense>
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Form} />
+            <Route path="/signup" exact component={Form} />
+          </Suspense>
         </Switch>
       </div>
-    </Router>
+    </div>
   )
 }
 
