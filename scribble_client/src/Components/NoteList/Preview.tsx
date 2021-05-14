b71ad4d0c68687de3bd145edcbf13cabb1a3afbc
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-14 22:06:27 +0530

    React gfm package installed and plugins added

--- scribble_client/src/Components/NoteList/Preview.tsx
+++ scribble_client/src/Components/NoteList/Preview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Markdown from 'react-markdown';
 import { FormProps } from '../../form.model';
+import gfm from 'remark-gfm';
 
 const Preview: React.FC<FormProps> = (props) => {
 
@@ -11,7 +12,9 @@ const Preview: React.FC<FormProps> = (props) => {
         <div>
             <div className="home_note_edit_title_input">{formField?.title ?? ''}</div>
             <div className="markdown_container">
-                <Markdown>
+                <Markdown
+                    remarkPlugins={[gfm]}
+                >
                     {formField?.content ?? ''}
                 </Markdown>
             </div>
