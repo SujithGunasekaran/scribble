9ed2ae1fe5d2e108acab76bcf0562e4be0ac72bd
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-14 21:35:41 +0530

    Login forms component added util components

--- scribble_client/src/UtilsComponent/index.tsx
+++ scribble_client/src/UtilsComponent/index.tsx
@@ -3,8 +3,17 @@ import { lazy } from 'react';
 const editnote = lazy(() => import('../Components/NoteList/EditNote'));
 const preview = lazy(() => import('../Components/NoteList/Preview'));
 
+// form component
+const login = lazy(() => import('../Components/Forms/LoginForm'));
+const signup = lazy(() => import('../Components/Forms/SignupForm'));
+
 export const components: any = {
     editnote,
     preview
 };
+
+export const formComponent: any = {
+    login,
+    signup
+}
 
