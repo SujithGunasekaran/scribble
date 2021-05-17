4713d247e3f6e508dba2dd00bc0ab8f69204804a
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-17 19:24:51 +0530

    form.model updated

--- scribble_client/src/form.model.ts
+++ scribble_client/src/form.model.ts
@@ -1,10 +1,18 @@
+import React from "react";
+
 export interface dynamicObject {
     [key: string]: any
 }
 
+export interface noteList {
+    noteList: any
+}
+
 export interface FormProps {
-    currentComponent: string,
-    formField: dynamicObject,
-    handleInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+    currentComponent?: string | undefined,
+    formError?: dynamicObject | undefined,
+    formField?: dynamicObject,
+    handleInputChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
     handleFormSubmit?: (e: React.FormEvent) => void
+    handleSaveForm?: (e: React.FormEvent) => void
 }
