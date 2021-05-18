49945f14bdaa52387a79fa23a7b2ce332ba53181
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-18 15:34:54 +0530

    User logout added

--- scribble_client/src/Components/Header/index.tsx
+++ scribble_client/src/Components/Header/index.tsx
@@ -1,8 +1,23 @@
-import React, { memo } from 'react';
-import { Link } from 'react-router-dom';
+import React, { memo, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import '../../Css/header.css';
 
 const Header: React.FC = memo(() => {
+
+    const [isUserLoggedOut, setIsUserLoggedOut] = useState<boolean>(false);
+
+    const userName = sessionStorage.getItem('email')?.split('@')[0];
+
+    const handleLogout = () => {
+        sessionStorage.clear();
+        setIsUserLoggedOut(true);
+    }
+
+    if (isUserLoggedOut) {
+        return <Redirect to="/" />
+    }
+
     return (
         <div>
             <div className="header_main">
@@ -14,12 +29,8 @@ const Header: React.FC = memo(() => {
                                     <Link to="/" href='/'>Scribble</Link>
                                 </div>
                                 <div className="header_auth_container">
-                                    <div className="header_login anchor_tag">
-                                        <Link to="/login" href="/login">Login</Link>
-                                    </div>
-                                    <div className="header_signup anchor_tag">
-                                        <Link to="/signup" href="/signup">Signup</Link>
-                                    </div>
+                                    <div className="header_username">Welcome, {userName}</div>
+                                    <div><ExitToAppIcon className="header_logout_icon" onClick={() => handleLogout()} /></div>
                                 </div>
                             </div>
                         </div>
